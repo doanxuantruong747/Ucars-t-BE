7609dc278da7c62f4f5ceea941e7e6c5ca0607e3
Truong <doanxuantruong747@gmail.com>
2022-11-30 14:16:31 +0700

    function Create Car Model

--- controllers/carModel.controller.js
+++ controllers/carModel.controller.js
@@ -6,6 +6,19 @@ const carModelController = {};
 
 // Create Car Model
 carModelController.createCarModel = catchAsync(async (req, res, next) => {
+ let { name } = req.body
+ let carModel = await CarModel.findOne({ name });
+
+ if (!name) {
+  throw new AppError(400, "Missing name", "Error Create Car Model")
+ }
+
+ if (carModel) {
+  throw new AppError(400, "car Model already exists", "Error Create Car Model")
+ }
+ carModel = await CarModel.create(({ name }))
+
+ sendResponse(res, 200, true, carModel, null, "Create new car Model successful")
 
 
 })
